49ca3a222523c7591a370edcf38c2e872606f548
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-02-13 13:50:30 -0800

    form consoleing

--- client/src/ContactForm.js
+++ client/src/ContactForm.js
@@ -1,57 +1,55 @@
 import React, {Component} from 'react';
-import axios from 'axios';
-import './form.css';
+// import {connect} from 'react-redux';
+// import {createPost} from '../actions/postActions';
 
 class ContactForm extends Component{
-    constructor(){
-      super()
-      this.state = {
-        name: '',
-        email: '',
-        message: ''
-      }
-      this.onChange = this.onChange.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
+  constructor(props){
+    super();
+    this.state = {
+      title: '',
+      body: ''
     }
-    onChange(e){
-  this.setState({[e.target.name]: e.target.value})
-}
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    onSubmit(e){
-      e.preventDefault();
-      const post = {
-        name:this.state.name,
-        email: this.state.email,
-        message:this.state.message
-      }
-      console.log('this is the post');
-      console.log(post);
-    }
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value})
+  }
 
-    render(){
-        return(
-            <div className="col-sm-4 offset-sm-4 form-wrapper">
-                <form id="contact-form" onSubmit={this.onSubmit} method="POST">
-                    <div className="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" className="form-control" id="name"
-                          onChange={this.onChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
-                         onChange={this.onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label for="message">Message</label>
-                        <textarea className="form-control" rows="2" id="message"
-                          onChange={this.onChange} />
-                    </div>
-                    <button type="submit" className="button1">Submit</button>
-                </form>
-            </div>
-        )
+  onSubmit(e){
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
     }
+    //call actions
+    console.log(post);
+  }
+
+
+  render() {
+    return(
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title:</label><br />
+            <input type="text" name="title"
+            value={this.state.title}
+            onChange={this.onChange}/>
+          </div>
+          <div>
+            <label>Body:</label><br />
+            <textarea name="body"
+            value={this.state.body}
+            onChange={this.onChange}/>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ContactForm;
