b3b4feaba545853a2078c5bf5656809fcf338a56
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-02-25 16:28:31 -0800

    as

--- client/src/About.js
+++ client/src/About.js
@@ -26,13 +26,14 @@ class About extends Component {
       <h4>Bio</h4>
       <p className="about-paragraph">I graduated from UC Santa Cruz in 2018 with a B.A. in Film and Digital Media. Since then I have gone on to attend Thinkful Coding Bootcamp where I have studied MondoDB, SQL, Node.js, Express.js, jQuery, and React.js. I also have some previous exprerience with Python and Django.</p>
         <DownloadLink
-      filename={require("./resume.pdf")}
+      filename="./resume.pdf"
       label = "Resume"
       exportFile={() => "My cached data"}
       style = {style}
       >
       Resume
   </DownloadLink>
+   <a href={require("./resume.pdf")} download>resume</a>
      </div>
     </div>
     </div>
