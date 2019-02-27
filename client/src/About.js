38fe220c766737512a77c18d958b8694f496adc7
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-02-27 10:42:30 -0800

    download button

--- client/src/About.js
+++ client/src/About.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import DownloadLink from "react-download-link";
 
 class About extends Component {
-
   render() {
-    let style = {
-      display: "inline-block",
-      background: "black",
-      color: "grey",
-      padding: "10px",
-      textDecoration: "none",
-      fontWeight: "bold"
-    };
     return (
     <div className="about-wrapper">
 
@@ -24,8 +14,8 @@ class About extends Component {
      </div>
      <div className="twoCol bio">
       <h4>Bio</h4>
-      <p className="about-paragraph">I graduated from UC Santa Cruz in 2018 with a B.A. in Film and Digital Media. Since then I have gone on to attend Thinkful Coding Bootcamp where I have studied MondoDB, SQL, Node.js, Express.js, jQuery, and React.js. I also have some previous exprerience with Python and Django.</p>
-      <a href={require("./resume.pdf")} download="Alec Livinghouse Resume">resume</a>
+        <p className="about-paragraph">I graduated from UC Santa Cruz in 2018 with a B.A. in Film and Digital Media. Since then I have gone on to attend Thinkful Coding Bootcamp where I have studied MondoDB, SQL, Node.js, Express.js, jQuery, and React.js. I also have some previous exprerience with Python and Django.</p>
+        <a href='./resume' download className="rain-button">Resume</a>
      </div>
     </div>
     </div>
