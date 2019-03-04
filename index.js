12558120aca75bed2bd9ad4f7225cc46ef1539a0
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-03-04 13:17:07 -0800

    deploy

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: false}));
 app.post('/api/form', (req, res) => {
   console.log('this is the body');
@@ -44,7 +43,10 @@ app.post('/api/form', (req, res) => {
     })
   })
 })
-
+app.use(express.static('client/build'))
+app.get('*', (req, res)=>{
+  res.sendFile(path.solve(__dirname, 'client', 'build', 'index.html'));
+});
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
