72e82cac27c878808bf2d5882ff93d4ba71df000
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-03-04 15:05:48 -0800

    asdf

--- client/src/About.js
+++ client/src/About.js
@@ -15,7 +15,7 @@ class About extends Component {
      <div className="twoCol bio">
       <h4>Bio</h4>
         <p className="about-paragraph">I graduated from UC Santa Cruz in 2018 with a B.A. in Film and Digital Media. Since then I have gone on to attend Thinkful Coding Bootcamp where I have studied MondoDB, SQL, Node.js, Express.js, jQuery, and React.js. I also have some previous exprerience with Python and Django.</p>
-        <a href='./resume.pdf' download className="rain-button">Resume</a>
+        <a href='./resume.pdf' download='./resume.pdf' className="rain-button">Resume</a>
      </div>
     </div>
     </div>
